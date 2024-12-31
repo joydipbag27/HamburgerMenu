b002b20dbc220f714b482cec4edd67758d5d5712
joydipbag27 <79678328+joydipbag27@users.noreply.github.com>
2024-12-31 13:28:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const menu = document.querySelector(".menu-icon");
+const popupMenu = document.querySelector(".popup-menu");
+const closeIcon = document.querySelector(".ri-close-fill");
+
+menu.addEventListener("click", () => {
+  popupMenu.classList.add("popup-menu-open");
+  menu.classList.add("menu-icon-hide");
+});
+
+closeIcon.addEventListener("click", () => {
+  if (
+    popupMenu.classList.contains("popup-menu-open") &&
+    menu.classList.contains("menu-icon-hide")
+  ) {
+    popupMenu.classList.remove("popup-menu-open");
+    menu.classList.remove("menu-icon-hide");
+  }
+});
