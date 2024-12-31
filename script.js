63c91643eb55e8f863472143282fa7c6a48b6fe0
joydipbag27 <79678328+joydipbag27@users.noreply.github.com>
2024-12-31 14:22:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,23 @@ const menu = document.querySelector(".menu-icon");
 const popupMenu = document.querySelector(".popup-menu");
 const closeIcon = document.querySelector(".ri-close-fill");
 
-menu.addEventListener("click", () => {
+menu.addEventListener("click", (e) => {
+  e.stopPropagation()
   popupMenu.classList.add("popup-menu-open");
   menu.classList.add("menu-icon-hide");
 });
 
 closeIcon.addEventListener("click", () => {
+  if (
+    popupMenu.classList.contains("popup-menu-open") &&
+    menu.classList.contains("menu-icon-hide")
+  ) {
+    popupMenu.classList.remove("popup-menu-open");
+    menu.classList.remove("menu-icon-hide");
+  }
+});
+
+window.addEventListener("click", () => {
   if (
     popupMenu.classList.contains("popup-menu-open") &&
     menu.classList.contains("menu-icon-hide")
